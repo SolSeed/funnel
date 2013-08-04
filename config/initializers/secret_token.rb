9f74858823a4230db2001780282fd1e4ab78bffe
Brandon CS Sanders <github.b@thesanders.us>
2013-08-03 23:03:59 -0700

    install vanilla rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funnel::Application.config.secret_key_base = '257798a9e187a15120bcf10daca07dfdca78868baf8635909191da51fdc8cab344e09cd49c8942eb4cfa33da97586ca35a640ecd714c69afa93e543648c5810f'
