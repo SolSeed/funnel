32e6b4e1526e13dc22e3d12fce9f1224bd63e34c
Brandon CS Sanders <github.b@thesanders.us>
2013-08-10 16:10:15 -0700

    the root route should always come first

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Funnel::Application.routes.draw do
-  devise_for :users
   root 'landing#index'
   resources :visitors
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
