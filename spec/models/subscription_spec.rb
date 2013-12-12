cfb3976b82aee5cd8fcd945c46eb5745e24c3f97
Brandon CS Sanders <brandon@thesanders.us>
2013-12-12 11:39:10 -0800

    require valid email addresses for subscriptions

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Subscription do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should be_a(Subscription) }
+
+  it "should reject a record with an invalid email" do
+    Subscription.new(email: "").should_not be_valid
+    Subscription.new(email: "blah").should_not be_valid
+    Subscription.new(email: "blah@foo").should_not be_valid
+    Subscription.new(email: "blah.foo.com").should_not be_valid
+  end
+
+  it "should create and accept records that have valid emails" do
+    Subscription.new(email: "test.b@thesanders.us").should be_valid
+  end
 end
